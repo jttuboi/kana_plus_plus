cb76da15a2b1407fd3a7cc1ed864f11632004a92
jttuboi <tuboi.jairo@gmail.com>
2021-07-24 17:00:00 -0300

    add selection of ios/android

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
+import 'dart:io';
+
 import "package:flutter/material.dart";
 import 'package:kana_plus_plus/src/repositories/shared_preferences_storage.provider.dart';
 import 'package:kana_plus_plus/src/shared/cache_storage.dart';
 import 'package:kana_plus_plus/src/views/android/android_app.dart';
+import 'package:kana_plus_plus/src/views/ios/ios_app.dart';
 
 void main() {
   // init cache storage before app
   initCacheStorage().then((value) {
-    runApp(AndroidApp());
-// TODO por enquanto utilizar o mesmo view para ambos
-//void main() => Platform.isIOS ? runApp(IOSApp()) : runApp(AndroidApp());
-// verificar todos os TODOs antes de fazer deploy para remover testes e ver se
-// não tem algo pendente
-// deixar o android no else fará qualquer outro que não seja ios ter a aparencia
-// do android, por exemplo, web ou windows
+    Platform.isIOS ? runApp(const IosApp()) : runApp(AndroidApp());
   });
 }
 
