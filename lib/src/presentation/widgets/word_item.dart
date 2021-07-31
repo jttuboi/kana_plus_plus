076c7f0233fc8929e8e50e2e45a34f53cccb7842
jttuboi <tuboi.jairo@gmail.com>
2021-07-31 06:35:45 -0300

    add simple background for letters in words

--- lib/src/presentation/widgets/word_item.dart
+++ lib/src/presentation/widgets/word_item.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import "package:flutter/material.dart";
 
 class WordItem extends StatelessWidget {
@@ -21,23 +23,59 @@ class WordItem extends StatelessWidget {
           alignment: Alignment.center,
           children: [
             Container(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(16.0),
               alignment: Alignment.topCenter,
               child: Hero(
                 tag: imageUrl,
                 child: Image.asset(imageUrl),
               ),
             ),
+            Container(),
             Container(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 24.0),
               alignment: Alignment.bottomCenter,
-              child: Text(
-                word,
-                style: const TextStyle(
-                  fontSize: 37,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
+              child: Stack(
+                children: [
+                  FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      word,
+                      style: TextStyle(
+                        fontSize: 37,
+                        fontWeight: FontWeight.bold,
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          ..strokeWidth = 10
+                          ..color = Colors.white.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                  FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      word,
+                      style: TextStyle(
+                          fontSize: 37,
+                          fontWeight: FontWeight.bold,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 7
+                            ..color = Colors.grey[200]! //.withOpacity(0.8),
+                          ),
+                    ),
+                  ),
+                  FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      word,
+                      style: const TextStyle(
+                        fontSize: 37,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
