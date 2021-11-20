3a99cc3a88562a00571e99e0b8a33d905f434fbf
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2021-11-20 20:50:28 -0300

    remove database to concentrate in box.

--- lib/features/training/infra/repositories/word.repository.dart
+++ lib/features/training/infra/repositories/word.repository.dart
@@ -2,58 +2,64 @@ import 'package:kwriting/core/core.dart';
 import 'package:kwriting/features/training/training.dart';
 
 class WordRepository implements IWordRepository {
+  //TODO modificar para box
   @override
   List<Word> getWords() {
-    final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
-    return File.storage.getWords().map((wordModel) {
-      wordModel.setLanguageCode(languageCode);
-      return wordModel;
-    }).toList();
+    return [];
+    // final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
+    // return File.storage.getWords().map((wordModel) {
+    //   wordModel.setLanguageCode(languageCode);
+    //   return wordModel;
+    // }).toList();
   }
 
   @override
   List<Word> getWordsById(String id) {
-    final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
-    return File.storage.getWordsById(id).map((wordModel) {
-      wordModel.setLanguageCode(languageCode);
-      return wordModel;
-    }).toList();
+    return [];
+    // final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
+    // return File.storage.getWordsById(id).map((wordModel) {
+    //   wordModel.setLanguageCode(languageCode);
+    //   return wordModel;
+    // }).toList();
   }
 
   @override
   List<Word> getWordsByQuery(String query) {
-    final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
-    return File.storage.getWordsByQuery(query, languageCode);
+    return [];
+    // final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
+    // return File.storage.getWordsByQuery(query, languageCode);
   }
 
   @override
   Word getWord(String id) {
-    final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
-    final word = File.storage.getWord(id)..setLanguageCode(languageCode);
-    return word;
+    return Word.empty();
+    // final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
+    // final word = File.storage.getWord(id)..setLanguageCode(languageCode);
+    // return word;
   }
 
   @override
   List<Word> getWordsForTraining(KanaType kanaType, int quantityOfWords) {
-    final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
+    return [];
+    //   final languageCode = Database.storage.getString(DatabaseTag.language, defaultValue: Default.locale);
 
-    if (kanaType.isBoth) {
-      final allWords = File.storage.getWords()..shuffle();
+    //   if (kanaType.isBoth) {
+    //     final allWords = File.storage.getWords()..shuffle();
 
-      final words = allWords.sublist(0, quantityOfWords);
-      for (final word in words) {
-        word.setLanguageCode(languageCode);
-      }
+    //     final words = allWords.sublist(0, quantityOfWords);
+    //     for (final word in words) {
+    //       word.setLanguageCode(languageCode);
+    //     }
 
-      return words;
-    }
-    final wordsByKanaType = File.storage.getWordsByKanaType(kanaType)..shuffle();
+    //     return words;
+    //   }
+    //   final wordsByKanaType = File.storage.getWordsByKanaType(kanaType)..shuffle();
 
-    final words = wordsByKanaType.sublist(0, quantityOfWords);
-    for (final word in words) {
-      word.setLanguageCode(languageCode);
-    }
+    //   final words = wordsByKanaType.sublist(0, quantityOfWords);
+    //   for (final word in words) {
+    //     word.setLanguageCode(languageCode);
+    //   }
 
-    return words;
+    //   return words;
   }
 }
